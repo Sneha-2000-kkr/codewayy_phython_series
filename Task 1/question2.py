5e255d61d8e4ccdb6ac565ff114cd30b5cba2b41
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-06-21 20:03:22 +0530

    Update question2.py

--- Task 1/question2.py
+++ Task 1/question2.py
@@ -1,45 +1,28 @@
->Program for display full name, college name, address, total marks and percentage.
-
-### Declaration of variables
-First_name = "Sneha "
+First_name = "Sneha "              //declaring variable related to personal details
 Last_name = "Singh "
 College_name = "Banasthali Vidyapeeth "
 Address = "Tonk, Rajasthan "
-
-### Full name using Concatenation :
-
 Full_name = First_name + Last_name
 print(Full_name)
-
-### college name and address :
-
-College_name_and_Address = (College_name + Address)
-print("Computer Technology from ",College_name + Address)
-
-
-### Marks Declaration:
-
-Marks_COA = 90
+College_name_and_Address = (College_name + Address)     // printing the strings using concatenation
+print("Computer Technology from ",College_name + Address)   // printing the strings using concatenation
+Marks_COA = 90            // declaring variables for marks of 5 subjects
 Marks_DAA = 90
 Marks_DS = 85
 Marks_DBMS = 91
 Marks_SP = 90
-
 print("\nMarks of five best subject: ")
 print("\n")
-print("Computer organisation and Architechure\t", Marks_COA)
+print("Computer organisation and Architechure\t", Marks_COA)     // printing the marks of the subjects
 print("Design and Anaylsis of Algorithm\t", Marks_DAA)
 print("Data Structure\t", Marks_DS)
 print("Data Base Managment System\t", Marks_DBMS)
 print("System Programming", Marks_SP)
 
-Total_marks = Marks_COA + Marks_DAA + Marks_DS + Marks_DBMS + Marks_SP
+Total_marks = Marks_COA + Marks_DAA + Marks_DS + Marks_DBMS + Marks_SP   // computing total marks scored
 print("\nTotal_marks : ",Total_marks)
-
-
-### Percentage :
-
 Total = 500
-Percentage = (Total_marks / Total) * 100
+Percentage = (Total_marks / Total) * 100    // computing percentage 
 print("Percentage = ",Percentage)
+
 
