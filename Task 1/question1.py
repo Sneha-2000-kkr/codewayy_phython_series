47dadc0b7b3c301c001afcdcc7ce95a4b2595ff0
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-06-21 19:26:42 +0530

    Create question1.py

---
+++ Task 1/question1.py
@@ -0,0 +1,23 @@
+### Implementing code via integer data types
+number1=int(input("Enter the first number:"))      //taking input from user for first number
+number2=int(input("Enter the second number:"))     //taking input from user for second number
+print("sum of two numbers",number1+number2)        //Addition of two the numbers which were taken from user
+print("difference of two numbers",number1-number2) //subtraction of two the numbers which were taken from user
+print("product of two numbers",number1*number2)    //multiplication of two the numbers which were taken from user
+print("comparison between two numbers")            //comparison of two the numbers which were taken from user via if-else statement 
+if(number1>number2):
+    print("number1 is greater than number2")
+else:
+    print("number2 is greater than number2")
+
+### Implementing code via float data types
+number1=float(input("Enter the first number:"))    //taking input from user for first number
+number2=float(input("Enter the second number:"))   //taking input from user for second number
+print("sum of two numbers",number1+number2)        //Addition of two the numbers which were taken from user
+print("difference of two numbers",number1-number2) //subtraction of two the numbers which were taken from user
+print("product of two numbers",number1*number2)    //multiplication of two the numbers which were taken from user
+print("comparison between two numbers")            //comparison of two the numbers which were taken from user via if-else statement 
+if(number1>number2):
+    print("number1 is greater than number2")
+else:
+    print("number2 is greater than number2")
