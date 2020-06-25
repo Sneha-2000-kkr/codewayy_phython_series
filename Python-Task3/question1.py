945a1dc9575b3f769d0aaa1ee93c7b3ed604353e
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-06-25 12:29:51 +0530

    details of the student

--- Python-Task3/question1.py
+++ Python-Task3/question1.py
@@ -1,63 +1,63 @@
 #1.a
 #function for returning full name of the student
-def MyNameFunc(FirstName,LastName):
-    FullName=FirstName + ' '+ LastName
-    return(FullName)
+def myNameFunc(firstName,lastName):
+    fullName=firstName + ' '+ lastName
+    return(fullName)
 
 #1.b
 #function for returning total marks scored by the student in all the subjects
-def TotalMarksFunc(ListofMarks):
-    TotalMarks=sum(ListofMarks)
-    return(TotalMarks)
+def totalMarksFunc(listOfMarks):
+    totalMarks=sum(listOfMarks)
+    return(totalMarks)
 
 #1.c
 #function for returning percentage scored by student
-def PercentageFunc(ListofMarks,TotalMaxMarks):
-    TotalMarks = sum(ListofMarks)
-    PercentageScored=(TotalMarks/TotalMaxMarks)*100
-    return(PercentageScored)
+def percentageFunc(listofMarks,totalMaxMarks):
+    totalMarks = sum(listOfMarks)
+    percentageScored=(totalMarks/totalMaxMarks)*100
+    return(percentageScored)
 
 #1.d
 #function for retuning grade of the student on basis of percentage scored
-def GradesScored(PercentageScored):
-    if(int(PercentageScored)>=95):
+def gradesScored(percentageScored):
+    if(int(percentageScored)>=95):
         return('A')
-    elif(int(PercentageScored)>=85 and int(PercentageScored)<=95):
+    elif(int(percentageScored)>=85 and int(percentageScored)<=95):
         return('B')
-    elif(int(PercentageScored)>=75 and int(PercentageScored)<=85):
+    elif(int(percentageScored)>=75 and int(percentageScored)<=85):
         return('C')
-    elif(int(PercentageScored)>=65 and int(PercentageScored)<=75):
+    elif(int(percentageScored)>=65 and int(percentageScored)<=75):
         return('D')
     else:
         return('FAILED')
 
 #1.e
 #function for calling all other functions and printing details of the student
-def DetailsOfStudent(FirstName,LastName,ListofMarks,TotalMaxMarks,PercentageScored):
-    print(MyNameFunc(FirstName,LastName ))
-    print(ListofMarks)
-    print("Total marked scored:", TotalMarksFunc(ListofMarks))
-    print("Percentage scored:", PercentageFunc(ListofMarks,TotalMaxMarks))
-    print("Grade Scored:", GradesScored(PercentageScored))
+def detailsOfStudent(firstName,lastName,listOfMarks,totalMaxMarks,percentageScored):
+    print(myNameFunc(firstName,lastName ))
+    print(listOfMarks)
+    print("Total marked scored:", totalMarksFunc(listOfMarks))
+    print("Percentage scored:", percentageFunc(listOfMarks,totalMaxMarks))
+    print("Grade Scored:", gradesScored(percentageScored))
 
 
 #taking required inputs from the user
-FirstName=str(input("Enter your first name\n"))
-LastName=str(input("Enter your last name\n"))
-NumberofSubjects = int(input("enter the no of subjects\n"))
-MaxMarkInEachSubj=int(input("enter max marks of each subject\n"))
-TotalMaxMarks=(NumberofSubjects*MaxMarkInEachSubj)
+firstName=str(input("Enter your first name\n"))
+lastName=str(input("Enter your last name\n"))
+numberOfSubjects = int(input("enter the no of subjects\n"))
+maxMarkInEachSubj=int(input("enter max marks of each subject\n"))
+totalMaxMarks=(numberOfSubjects*maxMarkInEachSubj)
 
 #initilasing a empty list of containing marks scored by the student
-ListofMarks = []
+listOfMarks = []
 
 #inputs from user regarding marks scored by student in each subject
 print("Enter the marks of each subject")
-for i in range(0, NumberofSubjects):
-        MarksofSubject = float(input())
-        ListofMarks.append(MarksofSubject)
-PercentageScored=PercentageFunc(ListofMarks,TotalMaxMarks)
+for i in range(0, numberOfSubjects):
+        marksOfSubject = float(input())
+        listOfMarks.append(marksOfSubject)
+percentageScored=percentageFunc(listofMarks,totalMaxMarks)
 
 #calling function to display all the details of the student
-DetailsOfStudent(FirstName,LastName,ListofMarks,TotalMaxMarks,PercentageScored)
+detailsOfStudent(firstName,lastName,listOfMarks,totalMaxMarks,percentageScored)
 
