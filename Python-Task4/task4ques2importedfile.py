58365f9f868762f6dd5c0c43606b813ad5a6d774
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-06-27 00:06:40 +0530

    Update task4ques2importedfile.py

--- Python-Task4/task4ques2importedfile.py
+++ Python-Task4/task4ques2importedfile.py
@@ -22,3 +22,39 @@ def sumOfAllElementsFromListFunc(listGiven):
         sumOfList=sumOfList+i
     return sumOfList
 
+def lengthOfString(givenString):
+    counter = 0
+    for s in givenString:
+       counter = counter+1
+    return counter
+
+def middleCharacterOfString(givenString):
+   return givenString[(lengthOfString(givenString)-1)//2:(lengthOfString(givenString)+2)//2]
+
+def wordsInString(givenString):
+    noOfWords=0
+    for i in givenString:
+        if i==" ":
+            noOfWords=noOfWords+1
+    return noOfWords+1
+
+
+def vowelsInString(givenString):
+    # Initializing count variable to 0
+    countOfVowels = 0
+
+    # Creating a set of vowels
+    vowelAlphabets = set("aeiouAEIOU")
+
+    # Loop to traverse the alphabet
+    # in the given string
+    for i in givenString:
+
+        # If alphabet is present
+        # in set vowel
+        if i in vowelAlphabets:
+            countOfVowels = countOfVowels + 1
+    return countOfVowels
+
+
+
