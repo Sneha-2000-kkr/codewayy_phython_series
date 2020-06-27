49f7844bb490ad72c720dc48e0f7362d958b060f
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-06-27 10:55:03 +0530

    defining logical operator

--- Python-Task4/task4ques2/logicalOperatorFunctions.py
+++ Python-Task4/task4ques2/logicalOperatorFunctions.py
@@ -1,25 +1,20 @@
 #2.c
 #defining function for computing and returning result on analysing two conditions with 'and' logical operator 
 def logicalOperatorAndFunc(numBer1,numBer2):
-    if(numBer1=='True'):
-        if(numBer2=='True'):
-            return ('True')
-    else:
-        return ('False')
+    if numBer1 > 10 and numBer2 > 10: 
+        print("Both the numbers are greater than 10") 
+    else: 
+        print("Both the numbers are smaller than 10")
     
 
 #defining function for computing and returning result on analysing two conditions with 'or' logical operator
 def logicalOperatorOrFunc(numBer1,numBer2):
-    if(numBer1=='False'):
-        if(numBer2=='False'):
-            return ('False')
-    else:
-        return('True')
+   if numBer1 > 10 or numBer2 > 10: 
+        print("Atleast one number is greater than 10") 
+    else: 
+        print("Both the numbers are smaller than 10")
     
     
 #defining function for computing and returning result on analysing a condition with 'not' logical operator
 def logicalOperatorNotFunc(numBer1):
-    if(numBer1=='True'):
-        return ('False')
-    else:
-        return ('True')
+   print(not(numBer1 > 5 and numBer1 < 10))
