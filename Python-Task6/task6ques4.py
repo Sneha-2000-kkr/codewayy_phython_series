d9f2a67a0d46bc1942bf643e61b8cc61089ee444
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 11:43:45 +0530

    counting occurrence of at in a string

--- Python-Task6/task6ques4.py
+++ Python-Task6/task6ques4.py
@@ -1,9 +1,17 @@
+
+# Defining function for counting occurence of at in the String irrespective of it's case
 def countOccurenceOfAt(stringGiven):
+    
+    # Initializing counter variable to zero
     countOfAt = 0
     for wordInString in stringGiven.split():
+        
+        # if at is a Word itself in the string
         if (wordInString == 'At' or wordInString == 'AT'):
             countOfAt = countOfAt + 1
         else:
+            
+            # If at is present in any word of the string
             wordList = []
             for charachterInWord in wordInString:
                 wordList.append(charachterInWord)
@@ -13,5 +21,7 @@ def countOccurenceOfAt(stringGiven):
                     countOfAt = countOfAt + 1
     return(countOfAt)
 
+
+# Taking string input from the user
 stringGiven=str(input("enter the string"))
 print("occurence of At in the String is ",countOccurenceOfAt(stringGiven))
