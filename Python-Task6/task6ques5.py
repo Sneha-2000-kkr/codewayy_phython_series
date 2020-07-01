0903f1d9374ce0c2fff76d10c44a006bcd9fddda
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 09:20:55 +0530

    Create task6ques5.py

---
+++ Python-Task6/task6ques5.py
@@ -0,0 +1,5 @@
+def noOfwordsInString(givenString):
+   return(len(givenString.split()))
+
+givenString=str(input("Enter the string"))
+print("no of words in string ",noOfwordsInString(givenString))
