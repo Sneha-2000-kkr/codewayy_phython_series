302c15f80d5080eca550ae330345ad23f4026937
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 11:35:34 +0530

    removing empty tuples from list

--- Python-Task6/task6ques2.py
+++ Python-Task6/task6ques2.py
@@ -1,11 +1,17 @@
+# list given in question
 listGiven=[(),(),('',),('a','b'),('a','b','c'),('d')]
+
+# Storing the index from the list which are to be deleted
 deleteList=[]
 print("Indexes and Elements which are to be deleted from list")
 for i in range(len(listGiven)):
     if(listGiven[i]==()):
         deleteList.append(i)
 
+#deleting desired indexes from the given list               
 for i in deleteList:
     print(i , listGiven.pop(deleteList[i-1]))
+    
+# Displaying desired list
 print("Desired List")
 print(listGiven)
