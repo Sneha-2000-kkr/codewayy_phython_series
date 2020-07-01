f6cc7762621aba131144f11095572b5a399948a5
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 11:39:33 +0530

    counting upper and lower cases in String

--- Python-Task6/task6ques3.py
+++ Python-Task6/task6ques3.py
@@ -1,13 +1,23 @@
+
+# Defining function for counting upper and lower case letters in the  string
 def countUpperLowerCase(stringGiven):
+    
+    #initlizing value for count for upper and lower case to zero
     countUpper = 0
     countLower= 0
+    
+    # Checking for upper cases and incrementing the counter variable accordingly 
     for i in stringGiven:
         if (i.isupper()):
             countUpper = countUpper + 1
     print(countUpper)
+    
+    # Checking for lower cases and incrementing the counter variable accordingly
     for i in stringGiven:
         if(i.islower()):
             countLower = countLower+1
     print(countLower)
+    
+ # Taking string input from the user   
 stringGiven=str(input("enter the string"))
 countUpperLowerCase(stringGiven)
