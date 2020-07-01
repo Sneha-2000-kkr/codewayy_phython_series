f8b3a59cbd03e0a0a2a523eb8c17953b629bafcc
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 11:50:29 +0530

    user defined exception

--- Python-Task6/task6ques7.py
+++ Python-Task6/task6ques7.py
@@ -1,8 +1,9 @@
+# User defined Exception
 class NotEligibleException(Exception):
     def message(self,message):
          return(self.message)
 
-
+# Checking for marks 
 def checkMarks(markGiven):
     try:
         if(markGiven>=90):
@@ -12,5 +13,6 @@ def checkMarks(markGiven):
     except NotEligibleException as no:
         return no
 
+ # taking marks as input from the user
 giveMark=int(input("Enter the mark "))
 print(checkMarks(giveMark))
