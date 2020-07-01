5adebce23e765af3b0a82fb926001199867dfaa3
Sneha-2000-kkr <66837936+Sneha-2000-kkr@users.noreply.github.com>
2020-07-01 11:46:02 +0530

    counting no of words in string

--- Python-Task6/task6ques5.py
+++ Python-Task6/task6ques5.py
@@ -1,5 +1,9 @@
+# defining function for counting number of words in the string
 def noOfwordsInString(givenString):
+   
+   #using split storing each word in the list , so length of list will give number of words in string
    return(len(givenString.split()))
 
+# taking string input from user
 givenString=str(input("Enter the string"))
 print("no of words in string ",noOfwordsInString(givenString))
